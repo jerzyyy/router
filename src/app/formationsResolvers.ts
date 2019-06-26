3ffad6576b8fc5a237088c0974cf1f66a8de4ed0
Yengo <jerzy.yengo@gmail.com>
2019-06-26 17:47:35 +0200

    suite

--- src/app/formationsResolvers.ts
+++ src/app/formationsResolvers.ts
@@ -1,23 +1,30 @@
-import {ActivatedRouteSnapshot, Resolve, RouterStateSnapshot} from '@angular/router';
-import {Observable} from 'rxjs';
-import {Injectable} from '@angular/core';
+import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Injectable } from '@angular/core';
 @Injectable()
 
 export class FormationsResolvers implements Resolve<any> {
-constructor() {
-}
+    constructor() {
+    }
 
 
-resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<any> | Promise<any> | any {
-return [{
-title: 'Module Angular',
-decription: '',
-}, {
-title: 'Module JavaScript',
-decription: '',
-}, {
-title: 'Module TypeScript',
-decription: '',
-}];
-}
+    resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<any> | Promise<any> | any {
+
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                resolve([
+                    {
+                        title: 'Module Angular',
+                        decription: '',
+                    }, {
+                        title: 'Module JavaScript',
+                        decription: '',
+                    }, {
+                        title: 'Module TypeScript',
+                        decription: '',
+                    },
+                ]);
+            }, 1000);
+
+        }
 }
